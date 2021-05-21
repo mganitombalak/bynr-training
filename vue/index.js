34e35365ed8f8d380b1f7179f88c9f41c4e35718
Mehmet Gani Tombalak <>
2021-05-21 12:34:27 +0300

    Vue Part I

---
+++ vue/index.js
@@ -0,0 +1,39 @@
+
+
+let app=Vue.createApp({
+    data() {
+        return {
+            tasks: [],
+            taskName: '',
+            options:{
+                buttonText:'Save',
+                title:'<strong>Tasks List</strong>'
+            },
+        };
+    },
+    methods: {
+        addTask(event,value) {
+            this.tasks.push(this.taskName);
+            this.taskName='';
+            console.dir(event);
+            console.log(`val:${value}`);
+            console.log(this.$refs.myText.value);
+        }
+    }
+});
+app.createComponent({});
+app.mount('#taskApp');
+
+//MVC, MVP, MVVM, MV *
+
+// let input = document.querySelector('#txtName');
+// let button = document.getElementById('button');
+// let list = document.getElementById('lstTask');
+
+// button.addEventListener('click', (event) => {
+//     console.dir(event);
+//     event.preventDefault();
+//     let newTask=document.createElement('li');
+//     newTask.textContent=input.value;
+//     list.appendChild(newTask);
+// });
