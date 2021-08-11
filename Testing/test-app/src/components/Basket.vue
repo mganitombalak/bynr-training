6dc704bb1ed4847719a1f1b38f8d6bb1fd6578a0
Mehmet Gani Tombalak <>
2021-08-11 21:50:11 +0300

    Reject

--- Testing/test-app/src/components/Basket.vue
+++ Testing/test-app/src/components/Basket.vue
@@ -1,9 +1,16 @@
 <template>
-  <ul>
+  <ul v-if="basketItems.length > 0">
     <li v-for="(basketItem, index) in basketItems" :key="'basketItem-' + index">
       {{ basketItem.name }} - {{ basketItem.qty }}
     </li>
   </ul>
+  <div v-if="basketItems.length <= 0">
+    <span>Sepetinizde henuz urun yok</span>
+    <a href="/shop">Alisverise devam edin!</a>
+    <!-- <router-link v-if="basketItems.length <= 0" :to="/shop"
+      >Alisverise devam edin!</router-link> -->
+  </div>
+  <div id="commError" v-if="hasError">It is not you. It is Us! Sorry :(</div>
 </template>
 <script>
 import axios from "axios";
@@ -19,7 +26,7 @@ export default {
       .post("/basket", {
         headers: { Authorization: "Bearer" + localStorage.getItem("token") },
       })
-      .then((result) => this.basketItems = result.data.items)
+      .then((result) => (this.basketItems = result.data.items))
       .catch((e) => (this.hasError = true));
   },
 };
--- Testing/test-app/tests/unit/basket.spec.js
+++ Testing/test-app/tests/unit/basket.spec.js
@@ -1,6 +1,7 @@
 // 1. Sepette Urun Varsa Urunlerin Listelenmesi
-// 2. Sepette urun yoksa Sepetiniz Bos metni goruntulenmeli
+// 2. Sepette urun yoksa Sepetiniz henuz urun yok metni goruntulenmeli her zaman ilk gorunmeli
 // 3. Sepette Urun yoksa Sepetiniz Bos metni altinda Alisverise devam etmek icin tiklayin! metni olmali
+//    Link arada her ne olursa olsun Hep en altta gorunmeli
 // 4. Herhangi bir sebeple API den sonuc alinamadigi zaman "Sorun sizde degil bizde ozur dileriz :(" 
 // metni goruntulenmeli hemen altinda "Bu problemden artik haberimiz var. Lutfen sepetinizi 
 // daha sonra tekrar kontrol edin"
@@ -10,11 +11,15 @@ import Basket from '../../src/components/Basket';
 let mockedPostFunction = jest.fn();
 jest.mock('axios', () => ({ post: () => mockedPostFunction() }))
 function wrapperFactory() {
-    return mount(Basket);
+    return mount(Basket,{
+        globa:{
+            stub:['router-link']
+        }
+    });
 }
 const localStorageMocketGet = jest.fn();
 window.localStorage.__proto__.getItem = localStorageMocketGet;
-describe.only('When basket has items, basket', () => {
+describe('When basket has items, basket', () => {
     beforeEach(() => {
         mockedPostFunction = jest.fn(() => Promise.resolve({
             data: {
@@ -31,23 +36,46 @@ describe.only('When basket has items, basket', () => {
             }
         }));
     });
-    it('should list items if in api response', async () => {
+    it('should list items if there is in api response', async () => {
         const wrapper = wrapperFactory();
         // expect(localStorageMocketGet).toHaveBeenCalledTimes(1);
         // expect(mockedPostFunction).toHaveBeenCalledTimes(1);
         await fp();
-        let actualCount=wrapper.findAll('ul>li').length;
+        let actualCount = wrapper.findAll('ul>li').length;
         let expectedPromise = await mockedPostFunction.mock.results[0].value;
         expect(actualCount).toBe(expectedPromise.data.items.length)
     });
 });
 
-describe('When basket no items, basket', () => {
-    it('should show \'Basket is Empty\' message if there is no item in list', () => { });
+describe('When basket  has no items, basket', () => {
+    beforeEach(() => {
+        mockedPostFunction = jest.fn(() => Promise.resolve({
+            data: {
+                items: []
+            }
+        }));
+    });
+    it('should show \'Basket is Empty\' message', () => {
+        const wrapper = wrapperFactory();
+        const firstElement= wrapper.find('div > span:first-of-type')
+        expect(firstElement.html()).toBe('<span>Sepetinizde henuz urun yok</span>');
+    });
 
-    it('should show \'Continue Shopping\' message if there is no item in list', () => { });
+    it('should show \'Continue Shopping\' message under \'Basket is empty\' message', () => {
+        const wrapper = wrapperFactory();
+        const lastElement = wrapper.find('div > a:last-of-type');
+        expect(lastElement.html()).toBe('<a href="/shop">Alisverise devam edin!</a>');
+    });
 });
 
-describe('When there is an comm error,basket', () => {
-    it('should show \'Sorry\' message if there is an error in API Communication', () => { });
+describe.only('When there is an comm error,basket', () => {
+    beforeEach(()=>{
+        mockedPostFunction = jest.fn(() => Promise.reject({status:500,message:'Comm Err'}));
+    });
+    it.only('should show \'Sorry\' message if there is an error in API Communication', async () => {
+        const wrapper = wrapperFactory();
+        await fp();
+        const commErrNotificationContainer= wrapper.find('div#commError');
+        expect(commErrNotificationContainer.html()).toContain('<div id=\"commError\">It is not you. It is Us! Sorry :(</div>');
+     });
 });
